351852fbd58f2f124f1f56d7658dac4b817db857
Manya7864 <manishyadav78642080@gmail.com>
2022-05-21 20:18:52 +0530

    todo

---
+++ todo/src/App.js
@@ -0,0 +1,12 @@
+import Todoapp from './component/todoapp';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Todoapp/>
+    </div>
+  );
+}
+
+export default App
